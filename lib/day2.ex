e1bd073db38bee7d3789f83cef0d0f7aa109591b
Kyle Baker <kbaker1@kabbage.com>
2017-12-04 22:06:45 -0500

    Add day2 examples

--- lib/day2.ex
+++ lib/day2.ex
@@ -14,6 +14,25 @@ end
 
 #------------------------------------------------------------------------------
 # Part One
+#
+# Given a table of data, find the checksum. The checksum is calculated by
+# summing the difference between the highest and lowest cell of each row.
+#
+# For example, given the following spreadsheet:
+#
+#  5 1 9 5
+#  7 5 3
+#  2 4 6 8
+#
+#  - The first row's largest and smallest values are 9 and 1, and their
+#    difference is 8
+#
+#  - The second row's largest and smallest values are 7 and 3, and their
+#    difference is 4
+#
+#  - The third row's difference is 6
+#
+#  In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18
 #------------------------------------------------------------------------------
 
 defmodule AdventOfCode2017.Day2.Part1 do
@@ -47,6 +66,25 @@ end
 
 #------------------------------------------------------------------------------
 # Part Two
+#
+# Find the only two numbers in each row where one evenly divides the other -
+# that is, where the result of the division operation is a whole number. Find
+# those numbers on each line, divide them, and add up each line's result.
+#
+# For example, given the following spreadsheet:
+#
+#  5 9 2 8
+#  9 4 7 3
+#  3 8 6 5
+#
+#  - In the first row, the only two numbers that evenly divide are 8 and 2; the
+#    result of this division is 4
+#
+#  - In the second row, the two numbers are 9 and 3; the result is 3
+#
+#  - In the third row, the result is 2
+
+#  In this example, the sum of the results (checksum) would be 4 + 3 + 2 = 9
 #------------------------------------------------------------------------------
 
 defmodule AdventOfCode2017.Day2.Part2 do
